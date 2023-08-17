8c22e11d9bd221812601cb1b5628265a2de16d19
Lerry Augusto <lerry.pirani@gmail.com>
2023-08-17 16:25:39 -0300

    puting transition to login_page from splash_screen

--- dw_barbershop/lib/src/barbershop_app.dart
+++ dw_barbershop/lib/src/barbershop_app.dart
@@ -1,5 +1,6 @@
 import 'package:asyncstate/widget/async_state_builder.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/barbershop_loader.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,7 @@ class BarbershopApp extends StatelessWidget {
           navigatorObservers: [asyncNavigatorObserver],
           routes: {
             '/': (_) => const SplashPage(),
+            '/auth/login': (_) => const LoginPage(),
           },
         );
       },
--- dw_barbershop/lib/src/features/splash/splash_page.dart
+++ dw_barbershop/lib/src/features/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -42,6 +43,25 @@ class _SplashPageState extends State<SplashPage> {
         child: Center(
           child: AnimatedOpacity(
             duration: const Duration(seconds: 3),
+            onEnd: () => Navigator.of(context).pushAndRemoveUntil(
+              PageRouteBuilder(
+                settings: const RouteSettings(name: '/auth/login'),
+                pageBuilder: (
+                  context,
+                  animation,
+                  secondaryAnimation,
+                ) {
+                  return const LoginPage();
+                },
+                transitionsBuilder: (_, animation, __, child) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  );
+                },
+              ),
+              (route) => false,
+            ),
             curve: Curves.easeIn,
             opacity: _animationOpacityLogo,
             child: AnimatedContainer(
